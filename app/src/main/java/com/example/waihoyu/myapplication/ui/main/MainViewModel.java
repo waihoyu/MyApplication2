af296f16282397efd986f1040f469190721b0ced
waihoyu <13701343809@qq.com>
2018-05-21 03:02:31 +0800

    '代码'

---
+++ app/src/main/java/com/example/waihoyu/myapplication/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.waihoyu.myapplication.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
